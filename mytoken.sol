cc0a24a4a6e9e0c1e452123fd1a2dd0fb43775be
HeeRyangChoi <90700073+HeeRyangChoi@users.noreply.github.com>
2021-11-22 22:02:45 +0900

    Update mytoken.sol

--- mytoken.sol
+++ mytoken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20{
-    uint public INITIAL_SUPPLY = 120000;
+    uint public INITIAL_SUPPLY = 120000000000000000000000;
     constructor() public ERC20("My Token","MT"){
         _mint(msg.sender, INITIAL_SUPPLY);
     }
